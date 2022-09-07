8a85caf850aca9bc11d7c1e88f4ac9b56cf2f12c
kirubelAtnafu <kiraatnafu@gmail.com>
2022-09-07 14:30:04 -0700

    bf

--- pchar.c
+++ pchar.c
@@ -1,29 +1,23 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
-
 /**
- * pchar - prints the int at the top of the stack as char
- * @stack: stack given by main
- * @line_cnt: amount of lines
- *
- * Return: void
+ * monty_pchar - A function that prints char at top value of the stack.
+ * @stack: The pointer to the stack list.
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
-void pchar(stack_t **stack, unsigned int line_cnt)
+void monty_pchar(stack_t **stack, unsigned int line_number)
 {
-	if (!stack || !(*stack))
-	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_cnt);
-		exit(EXIT_FAILURE);
-		return;
-	}
-	if (isascii((*stack)->n) == 0)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_cnt);
-		exit(EXIT_FAILURE);
-		return;
-	}
-	printf("%c\n", (*stack)->n);
+int number = 0;
+stack_t *tmp = *stack;
+if (!tmp)
+{
+f_errors(11, line_number);
+exit(EXIT_FAILURE);
+}
+number = tmp->n;
+if (number < 0 || number > 127)
+{
+f_errors(10, line_number);
+exit(EXIT_FAILURE);
+}
+printf("%c\n", tmp->n);
 }
