b6151d5d2132b09712bdaf61d7572e91cb7ba401
bereketkurkura <bereketkurkura@gmail.com>
2022-09-08 03:43:36 -0700

    Last

--- push.c
+++ push.c
@@ -1,14 +1,11 @@
 #include "monty.h"
-
 /**
  * f_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
  */
-
 void f_push(stack_t **head, unsigned int counter)
-
 {
 	int n, j = 0, flag = 0;
 
@@ -31,7 +28,7 @@ void f_push(stack_t **head, unsigned int counter)
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE); }
+		exit(EXIT_FAILURE);}
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		addnode(head, n);
